763d4e920bde7ab3a6e83d8315fcb99b69ce9f56
Dicey Devil <amin+git@worksity.com>
2025-04-09 10:34:53 +0100

    Game Score into loop

--- game_score.rb
+++ game_score.rb
@@ -1,9 +1,11 @@
-# Story: You're tallying points for a player's game session. Each level of the game gives a certain score, and you want to find the total score.
-# Problem: Write a method sum_scores(scores) that takes an array of numbers (each number is the score from a level) and returns the total sum of all scores. Use a loop (like .each or for) to accumulate the sum.
-
 def sum_scores(scores)
-  scores.sum { |score| score }
+  sum = 0
+  scores.each do |score|
+    sum = sum += score
+  end
+  sum
 end
 
 score = [1, 3, 5, 1]
+p sum_scores(score)
 puts sum_scores(score)
