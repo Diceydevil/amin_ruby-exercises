ef93059b55363df6cc578c600b1afcf8ea69fba9
Dicey Devil <amin+git@worksity.com>
2025-04-25 06:50:18 +0100

    csv reader

--- file_and_serialisation/csv.rb
+++ file_and_serialisation/csv.rb
@@ -5,3 +5,12 @@
 # Read and split each line into fields.
 # Sum total amount per region.
 # Write a new CSV sales_summary.csv with region,total_amount.
+
+require "csv"
+
+arr_of_rows = CSV.read("leads_list.csv", headers: true)
+arr_of_rows.each do |row|
+  puts "in #{row['Date Applied']}, #{row['Name']} applied for the position of bartending.\n
+  his email is: #{row['Email']}\n
+  his telephone is: #{row['Phone']}"
+end
